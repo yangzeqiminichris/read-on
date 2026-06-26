aac7c2bd5eed22e3ccf235da8f1f3ced9b1bb091
zeqi <914379475@qq.com>
2026-06-27 01:07:09 +0800

    feat: add chevron-down, trash-2, square-pen icons

--- test/icons.test.js
+++ test/icons.test.js
@@ -2,8 +2,10 @@ const test = require('node:test');
 const assert = require('node:assert');
 const icons = require('../src/lib/icons.js');
 
-test('icons.data 含 MVP 所需图标且为非空字符串', () => {
-  for (const id of ['bookmark', 'plus', 'play', 'rotate-cw', 'bookmark-plus']) {
+test('icons.data 含所需图标且为非空字符串', () => {
+  const ids = ['bookmark', 'plus', 'play', 'rotate-cw', 'bookmark-plus',
+               'chevron-down', 'trash-2', 'square-pen'];
+  for (const id of ids) {
     assert.ok(typeof icons.data[id] === 'string' && icons.data[id].length > 0, id);
   }
 });
